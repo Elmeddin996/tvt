b10a59896f690f51b6dad4a683fd05527860f8e0
Elmeddin Mirzeyev <118839615+Elmeddin996@users.noreply.github.com>
2026-07-06 21:22:35 +0400

    Initial solution architecture

---
+++ src/TVT.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace TVT.Core.Entities;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedDate { get; set; }
+    public bool IsActive { get; set; } = true;
+    public bool IsDeleted { get; set; } = false;
+}
---
+++ src/TVT.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TVT.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
